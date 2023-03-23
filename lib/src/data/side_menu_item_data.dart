5f1db7c0929b3e1521be04ddb2117981ff8496e1
Ahmed Hussien <83150642+AhmedH21@users.noreply.github.com>
2023-03-23 23:08:26 +0200

    Update side_menu_item_data.dart

--- lib/src/data/side_menu_item_data.dart
+++ lib/src/data/side_menu_item_data.dart
@@ -36,7 +36,7 @@ class SideMenuItemDataTile extends SideMenuItemData {
   final bool isSelected, hasSelectedLine;
   final void Function() onTap;
   final Size selectedLineSize;
-  final Widget? title;
+  final Text? title;
   final TextStyle? titleStyle;
   final String? tooltip;
   final Widget? badgeContent;
